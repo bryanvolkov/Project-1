b08701a7f90ddfd75cc3fe1d73ea7f7c2cd5e76e
bryanvolkov <bryan_2199@hotmail.com>
2018-09-28 11:39:11 -0700

    another commit

--- Project 1/Project 1/src/Driver.java
+++ Project 1/Project 1/src/Driver.java
@@ -1,20 +1,11 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Driver {
 	static Scanner scn;
 	public static void main(String[] args) {
+		LexicalAnalyzer lexer = new LexicalAnalyzer();
+	    lexer.tokenize("code.txt");
 
-	    File file = new File("code.txt"); 
-	    try {
-			scn = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} 
-	  
-	    while (scn.hasNextLine()) 
-	      System.out.println(scn.nextLine());
 	}
 
 }
